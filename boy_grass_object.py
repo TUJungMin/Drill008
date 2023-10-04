2339860e453e36c0ed2b6573ca70b6bc774f78b9
TUJungMin <paulkjm92@gmail.com>
2023-10-04 22:39:37 +0900

    공 객체 클래스 만들고 41*41 공크기로 랜덤좌표 만들어서 떨구기 성공!

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,6 +1,67 @@
 from pico2d import *
+import random
 
 # Game object class here
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = random.randint(0, 300), 90
+        self.frame = 0
+        self.image = load_image('run_animation.png')
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+
+
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+    def update(self):
+        pass
+
+class Ball:
+    def __init__(self):
+        self.x, self.y = random.randint(0, 800), 590
+        self.image = load_image('ball41x41.png')
+
+    def update(self):
+        self.y -= 5
+
+    def draw(self):
+        self.image.clip_draw(0, 0, 41, 41, self.x, self.y)
+
+
+def reset_world():
+    global grass
+    global team,Balls
+    global world
+    world = []
+    grass = Grass()
+    world.append(grass)
+    team = [Boy() for i in range(10)]
+    Balls = [Ball() for i in range(20)]
+    world += team
+    world += Balls
+
+
+def update_world():
+    for o in world:
+        o.update()
+
+
+def render_world():
+    clear_canvas()
+    for o in world:
+        o.draw()
+    update_canvas()
+
 
 def handle_events():
     global running
@@ -11,12 +72,21 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
+
+# 초기화 코드
 open_canvas()
 
-# initialization code
+# 변수 초기화
+running = True
+world = []
 
-# game main loop code
+# 게임 루프
+reset_world()
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.05)
 
-# finalization code
-
+# 종료 코드
 close_canvas()
